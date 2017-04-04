739d1f4d50161d8b97164663ef5604b33e36952a
Chibisi Chima-Okereke <chibisi@active-analytics.com>
2017-04-04 11:51:51 +0100

    added hemm template function

--- source/dblas/l3.d
+++ source/dblas/l3.d
@@ -138,17 +138,17 @@ void gemm(N, X)(in CBLAS_ORDER order, in CBLAS_TRANSPOSE transA, in CBLAS_TRANSP
         /* form  C := alpha*A*B + C */
         for (k = 0; k < K; k++) {
             for (i = 0; i < n1; i++) {
-            	static if(isComplex!X)
-                	    const X Fik = X(F[ldf*i + k].re, conjF*F[ldf*i + k].im);
-                	else
-                		const X Fik = F[ldf*i + k];
+                static if(isComplex!X)
+                        const X Fik = X(F[ldf*i + k].re, conjF*F[ldf*i + k].im);
+                    else
+                        const X Fik = F[ldf*i + k];
                 const X temp = alpha*Fik;
                 if (temp != zero) {
                     for (j = 0; j < n2; j++) {
-                    	static if(isComplex!X)
-                	        const X Gkj = X(G[ldg*k + j].re, conjG*F[ldg*k + j].im);
-                	    else
-                		    const X Gkj = G[ldg*k + j];
+                        static if(isComplex!X)
+                            const X Gkj = X(G[ldg*k + j].re, conjG*F[ldg*k + j].im);
+                        else
+                            const X Gkj = G[ldg*k + j];
                         c[ldc * i + j] += temp * Gkj;
                     }
                 }
@@ -163,14 +163,14 @@ void gemm(N, X)(in CBLAS_ORDER order, in CBLAS_TRANSPOSE transA, in CBLAS_TRANSP
             for (j = 0; j < n2; j++) {
                 X temp = zero;
                 for (k = 0; k < K; k++) {
-                	static if(isComplex!X){
-                	    const X Fik = X(F[ldf*i + k].re, conjF*F[ldf*i + k].im);
-                	    const X Gjk = X(G[ldg*j + k].re, conjG*G[ldg*j + k].im);
-                	}
-                	else{
-                		const X Fik = F[ldf*i + k];
-                		const X Gjk = G[ldg*j + k];
-                	}
+                    static if(isComplex!X){
+                        const X Fik = X(F[ldf*i + k].re, conjF*F[ldf*i + k].im);
+                        const X Gjk = X(G[ldg*j + k].re, conjG*G[ldg*j + k].im);
+                    }
+                    else{
+                        const X Fik = F[ldf*i + k];
+                        const X Gjk = G[ldg*j + k];
+                    }
                     temp += Fik*Gjk;
                 }
                 c[ldc * i + j] += alpha * temp;
@@ -181,17 +181,17 @@ void gemm(N, X)(in CBLAS_ORDER order, in CBLAS_TRANSPOSE transA, in CBLAS_TRANSP
     
         for (k = 0; k < K; k++) {
             for (i = 0; i < n1; i++) {
-            	static if(isComplex!X)
-                	    const X Fki = X(F[ldf*k + i].re, conjF*F[ldf*k + i].im);
-                	else
-                		const X Fki = F[ldf*k + i];
+                static if(isComplex!X)
+                        const X Fki = X(F[ldf*k + i].re, conjF*F[ldf*k + i].im);
+                    else
+                        const X Fki = F[ldf*k + i];
                 const X temp = alpha*Fki;
                 if (temp != zero) {
                     for (j = 0; j < n2; j++) {
-                    	static if(isComplex!X)
-                	        const X Gkj = X(G[ldg*k + j].re, conjG*G[ldg*k + j].im);
-                	    else
-                		    const X Gkj = G[ldg*k + j];
+                        static if(isComplex!X)
+                            const X Gkj = X(G[ldg*k + j].re, conjG*G[ldg*k + j].im);
+                        else
+                            const X Gkj = G[ldg*k + j];
                         c[ldc * i + j] += temp*Gkj;
                     }
                 }
@@ -204,14 +204,14 @@ void gemm(N, X)(in CBLAS_ORDER order, in CBLAS_TRANSPOSE transA, in CBLAS_TRANSP
             for (j = 0; j < n2; j++) {
                 X temp = zero;
                 for (k = 0; k < K; k++) {
-                	static if(isComplex!X){
-                	    const X Fki = X(F[ldf*k + i].re, conjF*F[ldf*k + i].im);
-                	    const X Gjk = X(G[ldg*j + k].re, conjG*G[ldg*j + k].im);
-                	}
-                	else{
-                		const X Fki = F[ldf*k + i];
-                		const X Gjk = G[ldg*j + k];
-                	}
+                    static if(isComplex!X){
+                        const X Fki = X(F[ldf*k + i].re, conjF*F[ldf*k + i].im);
+                        const X Gjk = X(G[ldg*j + k].re, conjG*G[ldg*j + k].im);
+                    }
+                    else{
+                        const X Fki = F[ldf*k + i];
+                        const X Gjk = G[ldg*j + k];
+                    }
                     temp += Fki * Gjk;
                 }
                 c[ldc * i + j] += alpha * temp;
@@ -225,6 +225,205 @@ void gemm(N, X)(in CBLAS_ORDER order, in CBLAS_TRANSPOSE transA, in CBLAS_TRANSP
 
 
 
+/** 
+*  @title gemm Computes a matrix-matrix product where one input matrix is Hermitian.
+*
+*  @description      The ?hemm routines compute a scalar-matrix-matrix product using a Hermitian matrix 
+*                    A and a general matrix B and add the result to a scalar-matrix product using a general
+*                    matrix C. The operation is defined as:
+*                    C := alpha*A*B + beta*C
+*                    or
+*                    C := alpha*B*A + beta*C,
+*                    where
+*
+*                    alpha and beta are scalars,
+*                    A is a Hermitian matrix,
+*                    B and C are m-by-n matrices.
+*
+*
+*  Input Parameters:
+*
+*  @param order:     Specifies whether two-dimensional array storage is row-major
+*                    (CblasRowMajor) or column-major (CblasColMajor).
+*
+*  @param side:      Specifies whether the Hermitian matrix A appears on the left or right in the
+*                    operation as follows:
+*                    if side = CblasLeft , then C := alpha*A*B + beta*C;
+*                    if side = CblasRight , then C := alpha*B*A + beta*C.
+*
+*  @param uplo:      Specifies whether the upper or lower triangular part of the Hermitian matrix
+*                    A is used:
+*                    If uplo = CblasUpper , then the upper triangular part of the Hermitian
+*                    matrix A is used.
+*                    If uplo = CblasLower , then the low triangular part of the Hermitian matrix
+*                    A is used.
+*
+*  @param m:         Specifies the number of rows of the matrix C.
+*                    The value of m must be at least zero.
+*
+*  @param n:         Specifies the number of columns of the matrix C.
+*                    The value of n must be at least zero.
+*
+*  @param alpha:     Specifies the scalar alpha.
+*
+*  @param a:         Array, size lda* ka , where ka is m when side = CblasLeft and is n
+*                    otherwise. Before entry with side = CblasLeft , the m-by-m part of the
+*                    array a must contain the Hermitian matrix, such that when uplo =
+*                    CblasUpper , the leading m-by-m upper triangular part of the array a must
+*                    contain the upper triangular part of the Hermitian matrix and the strictly
+*                    lower triangular part of a is not referenced, and when uplo = CblasLower ,
+*                    the leading m-by-m lower triangular part of the array a must contain the
+*                    lower triangular part of the Hermitian matrix, and the strictly upper
+*                    triangular part of a is not referenced.
+*                    Before entry with side = CblasRight , the n-by-n part of the array a must
+*                    contain the Hermitian matrix, such that when uplo = CblasUpper , the
+*                    leading n-by-n upper triangular part of the array a must contain the upper
+*                    triangular part of the Hermitian matrix and the strictly lower triangular part
+*                    of a is not referenced, and when uplo = CblasLower , the leading n-by-n
+*                    lower triangular part of the array a must contain the lower triangular part of
+*                    the Hermitian matrix, and the strictly upper triangular part of a is not
+*                    referenced. The imaginary parts of the diagonal elements need not be set,
+*                    they are assumed to be zero.
+*
+*  @param lda:       Specifies the leading dimension of a as declared in the calling (sub)
+*                    program. When side = CblasLeft then lda must be at least max(1, m) ,
+*                    otherwise lda must be at least max(1,n).
+*
+*  @param b:         For Layout = CblasColMajor : array, size ldb*n. The leading m-by-n part
+*                    of the array b must contain the matrix B.
+*
+*                    For Layout = CblasRowMajor : array, size ldb*m. The leading n-by-m part
+*                    of the array b must contain the matrix B
+*
+*  @param ldb:       Specifies the leading dimension of b as declared in the calling
+*                    (sub)program. When Layout = CblasColMajor , ldb must be at least
+*                    max(1, m) ; otherwise, ldb must be at least max(1, n).
+*
+*  @param beta:      Specifies the scalar beta.
+*                    When beta is supplied as zero, then c need not be set on input.
+*                    For Layout = CblasColMajor : array, size ldc*n . Before entry, the leading
+*                    m-by-n part of the array c must contain the matrix C, except when beta is
+*                    zero, in which case c need not be set on entry.
+*
+*  @param c:         For Layout = CblasRowMajor : array, size ldc*m . Before entry, the leading
+*                    n-by-m part of the array c must contain the matrix C, except when beta is
+*                    zero, in which case c need not be set on entry.
+*
+*  @param ldc:       Specifies the leading dimension of c as declared in the calling
+*                    (sub)program. When Layout = CblasColMajor , ldc must be at least
+*                    max(1, m) ; otherwise, ldc must be at least max(1, n).
+*
+*  Output Parameters:
+*  
+*  @param c:         Overwritten by the m-by-n updated matrix.
+*
+*/
+void hemm(N, X)(in CBLAS_ORDER order, in CBLAS_SIDE side, in CBLAS_UPLO uplo, in N m, in N n,
+                in X alpha, in X* a, in N lda, in X* b, in N ldb, in X beta, X* c, in N ldc)
+{
+    N i, j, k;
+    N n1, n2;
+    N uplo_, side_;
+    X zero = X(0), one = X(1);
+    
+    if(alpha == zero && beta == one)
+        return;
+    
+    if (order == CblasRowMajor) {
+        n1 = m;
+        n2 = n;
+        uplo_ = uplo;
+        side_ = side;
+    } else {
+        n1 = n;
+        n2 = m;
+        uplo_ = (uplo == CblasUpper) ? CblasLower : CblasUpper;
+        side_ = (side == CblasLeft) ? CblasRight : CblasLeft;
+    }
+    
+    /* form  y := beta*y */
+    if (beta == zero) {
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                c[ldc * i + j] = zero;
+            }
+        }
+    } else if (!(beta == one)) {
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                c[ldc*i + j] = beta*c[ldc*i + j];
+            }
+        }
+    }
+    
+    if (alpha == zero)
+        return;
+    
+    if (side_ == CblasLeft && uplo_ == CblasUpper) {
+        /* form  C := alpha*A*B + C */
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                const X Bij = b[ldb * i + j];
+                const X temp1 = alpha*Bij;
+                X temp2 = zero;
+                /* const BASE Aii_imag = 0.0; */
+                const X Aii = X(a[i*lda + i].re, 0);
+                c[i*ldc + j] += temp1 * Aii;
+                for (k = i + 1; k < n1; k++) {
+                    c[k*ldc + j] += conj(a[i*lda + k])*temp1;
+                    temp2 += a[i*lda + k]*b[ldb*k + j];
+                }
+                c[i*ldc + j] += alpha*temp2;
+          }
+        }
+    } else if (side_ == CblasLeft && uplo_ == CblasLower) {
+      /* form  C := alpha*A*B + C */
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                const temp1 = alpha*b[ldb*i + j];
+                X temp2 = zero;
+                for (k = 0; k < i; k++) {
+                    c[k*ldc + j] += conj(a[i*lda + k])*temp1;
+                    temp2 += a[i*lda + k]*b[ldb*k + j];
+                }
+                c[i*ldc + j] += temp1*a[i*lda + i].re;
+                c[i*ldc + j] += alpha*temp2;
+            }
+        }
+    } else if (side_ == CblasRight && uplo_ == CblasUpper) {
+        /* form  C := alpha*B*A + C */
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                const temp1 = alpha*b[ldb*i + j];
+                X temp2 = zero;
+                {
+                    c[i*ldc + j] += temp1*a[j*lda + j].re;
+                }
+                for (k = j + 1; k < n2; k++) {
+                    c[i*ldc + k] += temp1*a[j*lda + k];
+                    temp2 += conj(a[j*lda + k])*b[ldb*i + k];
+                }
+                c[i*ldc + j] += alpha*temp2;
+            }
+        }
+    } else if (side_ == CblasRight && uplo_ == CblasLower) {
+        /* form  C := alpha*B*A + C */
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                const X temp1 = alpha*b[ldb*i + j];
+                X temp2 = zero;
+                for (k = 0; k < j; k++) {
+                    c[i*ldc + k] = temp1*a[j*lda + k];
+                    temp2 += b[ldb*i + k]*conj(a[j*lda + k]);
+                }
+                c[i*ldc + j] += temp1*a[j*lda + j].re;
+                c[i*ldc + j] += alpha*temp2;
+            }
+        }
+    } else {
+        assert(0, "unrecognized operation");
+    }
+}
 
 
 
--- source/dblas/package.d
+++ source/dblas/package.d
@@ -20,29 +20,31 @@ import std.complex: Complex, complex;
 
 /* C function for gemm */
 extern (C){
-    void cblas_dgemm(const CBLAS_ORDER Order, const CBLAS_TRANSPOSE TransA,
-             const CBLAS_TRANSPOSE TransB, const int M, const int N,
-             const int K, const double alpha, const double *A, const int lda,
-             const double *B, const int ldb, const double beta, double *C,
-             const int ldc);
+    void
+cblas_zhemm (in CBLAS_ORDER Order, in CBLAS_SIDE Side,
+             in CBLAS_UPLO Uplo, in int M, in int N,
+             in void *alpha, in void *A, in int lda, in void *B,
+             in int ldb, in void *beta, void *C, in int ldc);
 }
 
-/* Testing for gemm */
+/* Testing for hemm */
 void main(){
     CBLAS_LAYOUT order = CblasRowMajor;
-    CBLAS_TRANSPOSE transA = CblasNoTrans;
-    CBLAS_TRANSPOSE transB = CblasTrans;
+    //CBLAS_TRANSPOSE transA = CblasNoTrans;
+    //CBLAS_TRANSPOSE transB = CblasTrans;
+    CBLAS_SIDE side = CblasLeft;
+    CBLAS_UPLO uplo = CblasUpper;
 
-    double[] a = [0.939, 0.705, 0.977, 0.4];
-    double[] b = [-0.089, -0.822, 0.937, 0.159, 0.789, -0.413, -0.172, 0.88];
-    double[] c = [-0.619, 0.063];
-    Complex!double[] ac = [complex(0.109, 0.892), complex(-0.723, 0.793), complex(0.109, -0.419), complex(-0.534, 0.448)];
-    Complex!double[] bc = [complex(-0.875, -0.31), complex(-0.027, 0.067), complex(0.274, -0.126), complex(-0.548, 0.497),
-                           complex(0.681, 0.388), complex(0.909, 0.889), complex(0.982, -0.074), complex(-0.788, 0.233)];
-    Complex!double[] cc = [complex(0.503, 0.067), complex(0.239, 0.876)];
-    gemm(order, transA, transB, 1, 2, 4, -0.3, a.ptr, 4, b.ptr, 4, 1, c.ptr, 2);
-    gemm(order, transA, transB, 1, 2, 4, complex(0., 0.1), ac.ptr, 4, bc.ptr, 4, complex(1., 0.), cc.ptr, 2);
-    writeln("gemm (double): ", c);
-    writeln("gemm (Complex!double): ", cc);
+    Complex!double[] a = [complex(-0.359, 0.089)];
+    Complex!double[] b = [complex(-0.451, -0.337), complex(-0.901, -0.871)];
+    Complex!double[] c = [complex(0.729, 0.631), complex(0.364, 0.246)];
+    Complex!double alpha = complex(0, 0.1), beta = alpha;
+
+    int ldc = 2, ldb = 2, lda = 1, m = 1, n = 2;
+    hemm(order, side, uplo, m, n, alpha, a.ptr, lda, b.ptr, ldb, beta, c.ptr, ldc);
+    writeln("hemm (Complex!double): ", c);
 }
+
+/*void hemm(N, X)(in CBLAS_ORDER order, in CBLAS_SIDE side, in CBLAS_UPLO uplo, in N m, in N n,
+                in X* alpha, in X* a, in N lda, in X* b, in N ldb, in X* beta, X* c, in N ldc)*/
 
